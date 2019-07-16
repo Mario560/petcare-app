e87d02dcb74520843d11720b5ce6bc8a9ca9f028
Mario560 <mario560lukec@gmail.com>
2019-07-16 20:40:27 +0200

    Timeformat bug fix

--- src/utils/cropOnlyTime.ts
+++ src/utils/cropOnlyTime.ts
@@ -2,7 +2,7 @@ const cropOnlyTime = (stringDateTime: string) => {
     if(stringDateTime.length > 10) {
         let dateInUTC = new Date(stringDateTime);
         dateInUTC.setHours(dateInUTC.getHours() + 2);
-        const time = dateInUTC.toLocaleTimeString();
+        const time = dateInUTC.toLocaleTimeString('en-GB', {hour12: false });
         return time.substring(0, 9);
     }
     return stringDateTime;
